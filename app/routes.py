7fd294625270e1e5ee26c6b9102802a8b96cbd56
brittneyjb <36802080+brittneyjb@users.noreply.github.com>
2019-10-26 11:34:21 -0400

    BASIC logic & placeholders for the index & login files

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,12 @@ from app import app
 from flask import render_template, redirect, url_for
 
 @app.route('/')
-@app.route('/index')
-def index(username=null):
-	if user == null:
-		return redirect(url_for(login))
-	return render_template('index.html', title='Home', user=user)
+@app.route('/index/username')
+def index(username=None):
+	if username == None:
+		return redirect('/login')
+	return render_template('index.html', title='Home', username=username)
 
 @app.route('/login')
 def login():
-
+	return render_template('login.html', title="Login")
