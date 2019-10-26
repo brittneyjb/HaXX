34803a6c90d84af5d9c5422441107635e066edf0
brittneyjb <36802080+brittneyjb@users.noreply.github.com>
2019-10-26 17:49:22 -0400

    LOGIN WORKS

--- app/models.py
+++ app/models.py
@@ -3,20 +3,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 
-class User(UserMixin, db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.String(64), primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-    	return '<User {}>'.format(self.username)    
+        return '<Users {}>'.format(self.id)    
 
     def set_password(self, password):
-    	self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-    	return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)
 
     @login.user_loader
     def load_user(id):
-    	return User.query.get(int(id))
+        return Users.query.get(id)
