38976a6767e46ad3815c2832121e22a8a0c4c8e0
brittneyjb <36802080+brittneyjb@users.noreply.github.com>
2019-10-26 15:13:23 -0400

    flask user login(not tested)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 import os
 import urllib.parse 
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+#from flask_migrate import Migrate
 import pyodbc
 
 params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=haxx.database.windows.net;DATABASE=UserDatabase;UID=haxx;PWD=HackGT2019")
@@ -9,10 +12,13 @@ params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=haxx.database.windo
 
 # initialization
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecret'
+app.config.from_object(Config)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+#migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,51 @@
 from app import app
+from app import db
 from flask import render_template, redirect, url_for
-
+from flask_bootstrap import Bootstrap
+import flask
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+from app.forms import LoginForm, RegistrationForms
 
 @app.route('/')
-@app.route('/index/username')
-def index(username=None):
-	if username == None:
-		return redirect('/login')
-	return render_template('index.html', title='Home', username=username)
+@app.route('/index')
+@login_required
+def index():
+	return render_template('index.html', title='Home')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html', title="Login")
+	form = LoginForm()
+	if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(id=form.id.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user():
+	return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(id=form.id.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
